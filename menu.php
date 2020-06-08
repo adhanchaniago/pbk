eb67f299e4576b4285f1ac9059eb28a8f1468455
iqbaltauf <50353320+iqbaltauf@users.noreply.github.com>
2020-06-08 22:43:10 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index1.php">keluar</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="siswa.html">kendaraan</a></li>
+			<li><a href="tabungan.html">total</a></li> 
+		</ul>
+	</div>
+</nav>
